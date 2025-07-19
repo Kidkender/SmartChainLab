9325399c152bcad5525e8d5d05c311b1641bfc75
kidkender <84608369+Kidkender@users.noreply.github.com>
2025-07-19 21:30:18 +0700

    fix build

--- src/components/functionForm.tsx
+++ src/components/functionForm.tsx
@@ -1,12 +1,10 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import type { ChainInfo } from "@/interfaces/network.type";
-import {
-  createInstruction,
-  getConnection,
-  sendTransaction,
-} from "@/lib/solanaClient";
+import { createInstruction, getConnection } from "@/lib/solanaClient";
 import { client as evmClient } from "@/lib/viemClient";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { Transaction } from "@solana/web3.js";
 import { useState } from "react";
 
 export function FunctionForm({
@@ -20,6 +18,8 @@ export function FunctionForm({
 }) {
   const [args, setArgs] = useState<string[]>([]);
   const [result, setResult] = useState<string>("");
+
+  const { publicKey, sendTransaction } = useWallet();
 
   const handleCall = async () => {
     if (chain.type === "evm") {
@@ -39,6 +39,10 @@ export function FunctionForm({
         setResult("❌ Error: " + (error as Error).message);
       }
     } else if (chain.type === "solana") {
+      if (!publicKey) {
+        setResult("❌ Please connect your wallet.");
+        return;
+      }
       try {
         const connection = getConnection(chain.rpcUrl!);
         const instruction = createInstruction({
@@ -51,13 +55,15 @@ export function FunctionForm({
           data: Buffer.from([]),
         });
 
-        const txid = await sendTransaction({
-          connection,
-          instructions: [instruction],
-          payer,
-        });
-        console.log("txid", txid);
-        setResult("✅ Tx sent: " + txid);
+        const transaction = new Transaction().add(instruction);
+        transaction.feePayer = publicKey;
+
+        try {
+          const txid = await sendTransaction(transaction, connection);
+          setResult("✅ Tx sent: " + txid);
+        } catch (error) {
+          setResult("❌ Error: " + (error as Error).message);
+        }
       } catch (error) {
         setResult("❌ Error: " + (error as Error).message);
       }
